4701679afb4fd4f68d7db3f7445573c50f11b80c
Iman-Fatima0 <iman.f8094@gmail.com>
2025-06-28 19:52:31 +0500

    gemini installed

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authControllers');
+const verifyToken = require('../utils/verifyToken');
+
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+
+router.get('/profile', verifyToken(), getProfile);
 
 module.exports = router;
